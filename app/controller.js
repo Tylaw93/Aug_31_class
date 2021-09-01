b591d090eac16c0e041b1f7b629385cacda4e45b
Tyler Lawson <tylaw93@yahoo.com>
2021-08-31 21:28:01 -0500

    Added functions to show and delete

--- app/controller.js
+++ app/controller.js
@@ -1,9 +1,22 @@
+/* eslint-disable new-cap */
 // Uses the db client from loader.js
-
+import { ObjectId } from "mongodb";
 import client from "./loader.js";
 
 export default {
+  index() {
+    return client.db("noteTaker").collection("notes").find({}).toArray();
+  },
   create(newNote) {
     return client.db("noteTaker").collection("notes").insertOne(newNote);
+  },
+  show(id) {
+    return client.db("noteTaker").collection("notes").findOne(ObjectId(id));
+  },
+  delete(id) {
+    return client
+      .db("noteTaker")
+      .collection("notes")
+      .deleteOne({ _id: ObjectId(id) });
   },
 };
