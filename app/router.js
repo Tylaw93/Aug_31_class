a62cf7380ce9835de34093eb6add507fcfad0fc0
Tyler Lawson <tylaw93@yahoo.com>
2021-08-31 20:44:23 -0500

    Adding routes

--- app/router.js
+++ app/router.js
@@ -3,5 +3,12 @@ import Router from "express";
 const router = new Router();
 
 // TODO: Add routes here (maybe 🤔 start with a GET test route)
+app.get('/', (_, res) =>{
+  res.send("Hello World from API")
+});
+
+router.post("/" , (req, res) =>{
+  res.json(req.body)
+} )
 
 export default router;
